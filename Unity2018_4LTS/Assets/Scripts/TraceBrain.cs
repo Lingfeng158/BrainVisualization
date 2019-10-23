f5dc151edea898ce62629dea1e8385a1c3df4d4b
LingfengLi <billylingfeng@outlook.com>
2019-10-22 22:25:42 -0500

    trace

--- Unity2018_4LTS/Assets/Scripts/TraceBrain.cs
+++ Unity2018_4LTS/Assets/Scripts/TraceBrain.cs
@@ -7,13 +7,14 @@ using Vector3 = UnityEngine.Vector3;
 
 public class TraceBrain : MonoBehaviour
 {
-    public Vector3 pos;
+    public Vector3Int pos;
     public string pathToFile;
 
-    List<Vector3> locations;
+    List<Vector3Int> locations;
 
     private int vecLength = 9;
     private double magnification = Mathf.Pow(10, 15);
+    private double step = 0.5;
     // Start is called before the first frame update
     void Start()
     {
@@ -29,7 +30,7 @@ public class TraceBrain : MonoBehaviour
      * internalKeeping: true location without rounding
      * return: a list of locations in image
      */
-    List<Vector3> traceBrain(Vector3 origin, System.Array tensorField, Vector3 internalKeeping)
+    List<Vector3Int> traceBrain(Vector3Int origin, System.Array tensorField, Vector3 internalKeeping)
     {
         int xDim = tensorField.GetUpperBound(0);
         int yDim = tensorField.GetUpperBound(1);
@@ -46,13 +47,53 @@ public class TraceBrain : MonoBehaviour
             List<Vector3> vectors = new List<Vector3>();
             list2vector(subArr, vectors);
             //find longest vector
+            float maxMag = -1;
+            Vector3 major=new Vector3();
+            int maxCounter = 1;
+            foreach(Vector3 i in vectors)
+            {
+                float tmp = i.magnitude;
+                if (tmp > maxMag)
+                {
+                    maxMag = tmp;
+                    major = i;
+                    maxCounter = 1;
+                }else if(tmp == maxMag)
+                {
+                    maxCounter += 1;
+                }
+            }
 
+            // empty voxel
+            if (maxMag == 0)
+            {
+                return new List<Vector3Int>();
+            }
+
+            // no major
+            if (maxCounter == 3)
+            {
+                return new List<Vector3Int>();
+            }
             //Question: how to proceed to next location? What should be the length of each vector?
-            
+
             //find next location and invoke transBrain recursively
-
+            Vector3 nextVoxel = internalKeeping;
+            nextVoxel.x = nextVoxel.x + (float)(major.x * step);
+            nextVoxel.y = nextVoxel.y + (float)(major.y * step);
+            nextVoxel.z = nextVoxel.z + (float)(major.z * step);
+            Vector3Int externalVoxel = new Vector3Int((int)(nextVoxel.x), (int)(nextVoxel.y), (int)(nextVoxel.z));
             //get result from recursion and add current location AT FRONT
-
+            if (externalVoxel.Equals(origin))
+            {
+                return traceBrain(externalVoxel, tensorField, nextVoxel);
+            }
+            else
+            {
+                var result = traceBrain(externalVoxel, tensorField, nextVoxel);
+                result.Insert(0, externalVoxel);
+                return result;
+            }
 
 
 
@@ -60,7 +101,7 @@ public class TraceBrain : MonoBehaviour
         }
         else
         {
-            return new List<Vector3>();
+            return new List<Vector3Int>();
         }
 
     }
