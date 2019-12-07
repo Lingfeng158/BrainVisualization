ee6950890d6dc2eb17cbcb3cd70e36a43bd7dd70
Ohm Patel <2ompatel@gmail.com>
2019-12-07 12:19:34 -0600

    rotate parent object

---
+++ Unity2018_4LTS/Assets/Scripts/rotateObject.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    float rotSpeed = 20;
+
+    void OnMouseDrag()
+    {
+        float rotX = Input.GetAxis("Mouse X") * rotSpeed * Mathf.Deg2Rad;
+        float rotY = Input.GetAxis("Mouse Y") * rotSpeed * Mathf.Deg2Rad;
+
+        GameObject[] gos = GameObject.FindGameObjectsWithTag("RotationCenter");
+        foreach (GameObject go in gos)
+        {
+            go.transform.RotateAround(Vector3.up, -rotX);
+            go.transform.RotateAround(Vector3.right, rotY);
+        }
+
+        
+    }
+
+}
